e85937ed1a5ecf02e509a56b2b77dd557728bae8
telegram-bot98 <ilyxervy@gmail.com>
2025-09-04 15:58:20 +0300

    Create phrases.py

---
+++ bot/phrases.py
@@ -0,0 +1,15 @@
+phrases = [
+    # عربي
+    "مرحباً فريق دعم إنستغرام، تم حظر حسابي عن طريق الخطأ، أرجو مراجعته وإعادة تفعيله. شكراً لكم.",
+    "تحية طيبة، أواجه مشكلة في تسجيل الدخول إلى حسابي على إنستغرام، أرجو المساعدة بأقرب وقت ممكن.",
+    "لقد تم تعطيل حسابي فجأة رغم التزامي بجميع سياسات إنستغرام، أطلب منكم مراجعة الحظر.",
+    "أستخدم حسابي للتواصل مع عائلتي وأصدقائي فقط، أرجو إعادة تفعيله، شكراً لتعاونكم.",
+    "تم حظر حسابي بشكل خاطئ، أرجو مساعدتي في استعادته، يمكنكم التواصل معي عبر البريد الإلكتروني إذا لزم الأمر.",
+
+    # إنجليزي
+    "Hello Instagram Support Team, my account was disabled by mistake. Kindly review and reactivate it. Thank you.",
+    "Hi, I'm facing issues logging into my Instagram account. Please assist me in resolving this problem.",
+    "My account has been disabled unexpectedly, although I follow all Instagram guidelines. Please review this case.",
+    "I use my account only to communicate with family and friends. Please help me recover it.",
+    "My account was wrongly banned, I kindly request your assistance to restore it. Thank you."
+]
